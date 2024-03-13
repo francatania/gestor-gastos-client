27314dccc4b0fac71c562543a8064d512660c960
Franco Catania <francocatania93@gmail.com>
2024-03-12 22:53:06 -0300

    ctrl z al commit anterior

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  server: {
+    // Configuración para manejar rutas desconocidas redirigiendo a index.html
+    historyApiFallback: true,
+  }
 })
 
 
