3fb4ae2e72fab3e4201766abbcd73ca17e5db08d
Franco Catania <francocatania93@gmail.com>
2024-03-12 22:45:18 -0300

    Se eliminar parte del vite config para probar

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    // Configuración para manejar rutas desconocidas redirigiendo a index.html
-    historyApiFallback: true,
-  }
+
 })
 
 
